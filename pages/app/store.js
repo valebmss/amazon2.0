ad567e5ed1587d6caa1b439e089bd626fada29e0
Valeria Larrea <leidyvale244@gmail.com>
2022-12-21 21:30:38 -0500

    all

--- pages/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import basketReducer from "../src/slices/basketSlices";
-
-export const store = configureStore({
-    reducer: {
-        basket: basketReducer,
-    },
-});
